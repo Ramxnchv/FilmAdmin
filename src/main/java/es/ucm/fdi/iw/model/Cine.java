988c257aeb90b4e959585731069c8a7eadad7f94
Ramón Rosa Pérez <ramonros@ucm.es>
2022-02-28 14:33:41 +0100

    Clase Cine con JPA funcionando

--- src/main/java/es/ucm/fdi/iw/model/Cine.java
+++ src/main/java/es/ucm/fdi/iw/model/Cine.java
@@ -1,9 +1,21 @@
+package es.ucm.fdi.iw.model;
 
+import javax.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
+@Entity
+@Data
+@NoArgsConstructor
 public class Cine {
 
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "gen")
+    @SequenceGenerator(name = "gen", sequenceName = "gen")
     private long id;
+
     private String horario;
     private String telefono;
     private String direccion;
@@ -11,60 +23,15 @@ public class Cine {
     private String urlmap;
 
 
-    public Cine(long id, String horario, String telefono, String direccion, String ciudad, String urlmap) {
-        this.id = id;
-        this.horario = horario;
-        this.telefono = telefono;
-        this.direccion = direccion;
-        this.ciudad = ciudad;
-        this.urlmap = urlmap;
+    @Getter
+    @AllArgsConstructor
+    public static class Transfer {
+		private long id;
+        private String horario;
+        private String telefono;
+        private String direccion;
+        private String ciudad;
+        private String urlmap;
     }
 
-    public String getUrlmap() {
-        return urlmap;
-    }
-
-    public void setUrlmap(String urlmap) {
-        this.urlmap = urlmap;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getHorario() {
-        return horario;
-    }
-
-    public void setHorario(String horario) {
-        this.horario = horario;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
-
-    public String getDireccion() {
-        return direccion;
-    }
-
-    public void setDireccion(String direccion) {
-        this.direccion = direccion;
-    }
-
-    public String getCiudad() {
-        return ciudad;
-    }
-
-    public void setCiudad(String ciudad) {
-        this.ciudad = ciudad;
-    }
 }
