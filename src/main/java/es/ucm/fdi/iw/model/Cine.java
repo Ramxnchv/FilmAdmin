b99117da9b607a63b1664d7434cd6a98b807ba5b
Ramón Rosa Pérez <ramonros@ucm.es>
2022-03-01 13:59:05 +0100

    Añadidos días apertura y cierre en Cine

--- src/main/java/es/ucm/fdi/iw/model/Cine.java
+++ src/main/java/es/ucm/fdi/iw/model/Cine.java
@@ -1,9 +1,14 @@
 package es.ucm.fdi.iw.model;
 
+import java.time.DayOfWeek;
 import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import javax.persistence.*;
+
+import org.hibernate.annotations.CollectionType;
+
 import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -24,6 +29,13 @@ public class Cine implements Transferable<Cine.Transfer> {
 
     @OneToMany(targetEntity = Sesion.class)
     private List<Sesion> sesiones = new ArrayList<>();
+
+    @ElementCollection
+    private List<DayOfWeek> dias_apertura = new ArrayList<>();
+
+    @ElementCollection
+    private List<Date> festivos_cierre = new ArrayList<>();
+
     private LocalTime hora_apertura;
     private LocalTime hora_cierre;
     private String telefono;
@@ -38,6 +50,8 @@ public class Cine implements Transferable<Cine.Transfer> {
 		    private long id;
         private List<Sala> salas;
         private List<Sesion> sesiones;
+        private List<DayOfWeek> dias_apertura;
+        private List<Date> festivos_cierre;
         private LocalTime hora_apertura;
         private LocalTime hora_cierre;
         private String telefono;
@@ -49,7 +63,7 @@ public class Cine implements Transferable<Cine.Transfer> {
 
     @Override
     public Transfer toTransfer() {
-		return new Transfer(id,	salas, sesiones, hora_apertura, hora_cierre, telefono, direccion, ciudad, urlmap);
+		return new Transfer(id,	salas, sesiones, dias_apertura, festivos_cierre, hora_apertura, hora_cierre, telefono, direccion, ciudad, urlmap);
 	}
 	
 	@Override
