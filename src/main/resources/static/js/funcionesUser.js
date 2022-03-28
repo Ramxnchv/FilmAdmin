5bb2eba162e634f9efc6545a46499831f453843e
Daniel <danilu01@ucm.es>
2022-03-28 05:39:15 +0200

    Modal de editar usuario funcionando

--- src/main/resources/static/js/funcionesUser.js
+++ src/main/resources/static/js/funcionesUser.js
@@ -110,12 +110,48 @@ $(function () {
         $modal.find('input[id="nombre"]').val(firstname);
         $modal.find('input[id="apellidos"]').val(lastname);
         $modal.find('input[id="fecha"]').val(birthdate);
-        
+
+        $modal.find('input[id="username"]').prop('required', false);
+        $modal.find('input[id="email"]').prop('required', false);
+        $modal.find('input[id="password"]').prop('required', false);
+        $modal.find('input[id="confirm_password"]').prop('required', false);
+        $modal.find('input[id="nombre"]').prop('required', false);
+        $modal.find('input[id="apellidos"]').prop('required', false);
+        $modal.find('input[id="fecha"]').prop('required', false);
+
         const action = $modal.find('form').attr('action').replace('-1', id);
-        $modal.find('form').attr('action',action);
+        $modal.find('form').attr('action', action);
+
+        $modal.find('form').submit(function(event) {
+            event.preventDefault();
+            
+            const $form = $(this);
+
+            const username = $form.find('input[id="username"]').val();
+
+            const email = $form.find('input[id="email"]').val();
+            if (email) $modal.find('form').attr('action', action);
+
+            const password = $form.find('input[id="password"]').val();
+            const pass2 = $form.find('input[id="confirm_password"]').val();
+
+            const firstName = $form.find('input[id="nombre"]').val();
+            if (firstName) $btn.attr('data-firstname', firstName);
+
+            const lastName = $form.find('input[id="apellidos"]').val();
+            if (lastName) $btn.attr('data-lastname', lastName);
+
+            const birthDate = $form.find('input[id="fecha"]').val();
+            if (birthDate) $btn.attr('data-birthdate', birthDate);
+           
+            let data = { username, email, password, pass2, firstName, lastName, birthDate}
+
+            $modal.modal('hide');
+    
+            go(action, "POST", data);
+    
+          });
 
         $modal.modal('show');
     });
-
-
 });
